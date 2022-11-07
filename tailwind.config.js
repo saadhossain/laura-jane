1250e21df462b176736da501325d93d0506996e3
saadhossain <mshsarkar@gmail.com>
2022-11-07 21:38:11 +0600

    App Installed on Client site and installed necessary packages

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+          colors: {
+            "just": "#ed7966",
+            "just2nd": "#303179"
+          }
+        },
+    },
+    plugins: [],
+}
